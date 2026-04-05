7c221d03fa42f011b11b56dc05a1e1b648a613ec
shanid <shanid77shan@gmail.com>
2026-04-06 02:47:17 +0530

    new

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.shanid.aiinvitation',
+  appName: 'AI Invitation',
+  webDir: 'dist'
+};
+
+export default config;
